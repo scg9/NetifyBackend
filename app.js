831ba343d5fe574aec795291c5ddfb748f31cf2b
LAPTOP-CC4VON0K\swaga <chawlaswagat@gmail.com>
2024-12-16 18:04:32 +0530

    for deployment

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
     }
   });
 });
+app.get("/", (req, res) => { res.send("Express on Vercel"); });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
